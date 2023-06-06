348b9ca850d027723b458e5c6d8b61f2003dfb5d
suraj gaire <eriag321@gmail.com>
2023-06-06 13:55:23 +0545

    member page added

--- src/app/members/page.jsx
+++ src/app/members/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import style from "../styles/members.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 const Members = () => {
   const members = [
@@ -9,13 +11,73 @@ const Members = () => {
       title: "President",
       links: [
         {
-          link: <i className="ri-facebook-line"></i>,
+          link: "https://www.facebook.com",
+          logo: <i className="ri-facebook-line"></i>,
         },
         {
-          link: <i className="ri-instagram-line"></i>,
+          link: "https://www.instagram.com/",
+          logo: <i className="ri-instagram-line"></i>,
         },
         {
-          link: <i className="ri-linkedin-line"></i>,
+          link: "https://www.linkedin.com/",
+          logo: <i className="ri-linkedin-line"></i>,
+        },
+      ],
+    },
+    {
+      img: "https://images.unsplash.com/photo-1685452329316-d505335ca3d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      name: "Sudeep Kumar Mishra",
+      title: "President",
+      links: [
+        {
+          link: "https://www.facebook.com",
+          logo: <i className="ri-facebook-line"></i>,
+        },
+        {
+          link: "https://www.instagram.com/",
+          logo: <i className="ri-instagram-line"></i>,
+        },
+        {
+          link: "https://www.linkedin.com/",
+          logo: <i className="ri-linkedin-line"></i>,
+        },
+      ],
+    },
+    {
+      img: "https://images.unsplash.com/photo-1685452329316-d505335ca3d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      name: "Sudeep Kumar Mishra",
+      title: "President",
+      links: [
+        {
+          link: "https://www.facebook.com",
+          logo: <i className="ri-facebook-line"></i>,
+        },
+        {
+          link: "https://www.instagram.com/",
+          logo: <i className="ri-instagram-line"></i>,
+        },
+        {
+          link: "https://www.linkedin.com/",
+          logo: <i className="ri-linkedin-line"></i>,
+        },
+      ],
+    },
+    {
+      img: "https://images.unsplash.com/photo-1685452329316-d505335ca3d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      name: "Sudeep Kumar Mishra",
+      title: "President",
+      links: [
+        {
+          link: "https://www.facebook.com",
+          logo: <i className="ri-facebook-line"></i>,
+        },
+        {
+          link: "https://www.instagram.com/",
+          logo: <i className="ri-instagram-line"></i>,
+        },
+        {
+          link: "https://www.linkedin.com/",
+          logo: <i className="ri-linkedin-line"></i>,
         },
       ],
     },
@@ -32,6 +94,32 @@ const Members = () => {
       <div className={style.executive__title}>
         <h2>Executive Members</h2>
         <p>Leading the Way in our ICT Club</p>
+      </div>
+      {/* <div className={style.year_tabs}  >
+        <button>2023</button>
+      </div> */}
+      <div className={style.executive__members}>
+        {members.map((member, i) => {
+          return (
+            <div key={i} className={style.member}>
+              <Image
+                src={member.img}
+                width={120}
+                height={120}
+                alt="executive member"
+              />
+              <h3>{member.name}</h3>
+              <h4>{member.title}</h4>
+              {member.links.map((link, i) => {
+                return (
+                  <div key={i} className={style.links}>
+                    <Link href={link.link}>{link.logo}</Link>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
